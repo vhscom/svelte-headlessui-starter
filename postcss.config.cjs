c4a7aaab5e21fc81c122c516df18a7d9390e4b23
vhs <vhsdev@tutanota.com>
2022-02-16 22:48:41 +0800

    feat: ✨ install and configure cssnano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,19 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const prod = process.env.NODE_ENV === 'production';
 
 const config = {
 	plugins: [
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
-		autoprefixer
+		autoprefixer,
+		prod &&
+			cssnano({
+				preset: 'default'
+			})
 	]
 };
 
