e5fd9139a2e366c672626b28cd0e7e21e038829b
vhs <vhsdev@tutanota.com>
2022-02-21 19:32:35 +0800

    feat: ✨ customize font stack

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
 const typography = require('@tailwindcss/typography');
 const forms = require('@tailwindcss/forms');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 
 	plugins: [forms, typography]
