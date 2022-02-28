1eb24f4db3ad3d0f8bf613bb7200dd4d4994b790
vhs <vhsdev@tutanota.com>
2022-02-28 19:46:39 +0800

    fix: 🐛 disable supabase by default

--- src/routes/api/navigation.ts
+++ src/routes/api/navigation.ts
@@ -1,13 +1,13 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { navigationData } from '$data';
-import { supabase } from '$core/services/supabase';
-import { NavItemModel } from '$models/classes/nav-item.model';
+// import { supabase } from '$core/services/supabase';
+// import { NavItemModel } from '$models/classes/nav-item.model';
 
 export const get: RequestHandler = async () => {
-	const res = await supabase.from('navigation').select('*');
-	const data = res.data ? res.data.map((value) => new NavItemModel().deserialize(value)) : null;
+	// const res = await supabase.from('navigation').select('*');
+	// const data = res.data ? res.data.map((value) => new NavItemModel().deserialize(value)) : null;
 
 	return {
-		body: JSON.stringify(data || navigationData)
+		body: JSON.stringify(navigationData)
 	};
 };
