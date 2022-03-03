5703e1820b1efcfdd6aa836ce36737703a29b5a7
vhs <vhsdev@tutanota.com>
2022-03-04 02:34:01 +0800

    refactor: ♻️ buttonize icon button

--- src/lib/shared/components/form/IconButton.svelte
+++ src/lib/shared/components/form/IconButton.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import FallbackIcon from '~icons/heroicons-outline/plus-circle';
+
 	export let href: string;
 	export let icon = FallbackIcon;
+
+	const activate = (target: EventTarget) => {
+		target instanceof HTMLAnchorElement && goto(target.href);
+	};
+
+	const handleClick = (event: MouseEvent) => {
+		event instanceof PointerEvent && event.pointerType && activate(event.target);
+	};
+
+	const handleKeyup = (event: KeyboardEvent) => {
+		switch (event.code) {
+			case 'Enter':
+			case 'Space':
+				activate(event.target);
+			default:
+				return;
+		}
+	};
 </script>
 
 <a
+	role="button"
+	on:click|preventDefault={handleClick}
+	on:keyup={handleKeyup}
 	{href}
 	class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-3 py-2 text-sm font-medium text-indigo-900 no-underline hover:bg-indigo-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
 >
