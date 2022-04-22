c5621d60e63dd75d54ee34bdd904729dfa5ac71e
vhs <vhsdev@tutanota.com>
2022-04-22 12:58:54 +0800

    chore(navigation): 🔧 hydrate with mockdata while debugging only

--- src/routes/api/navigation.ts
+++ src/routes/api/navigation.ts
@@ -1,13 +1,24 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { navigationData } from '$data';
-// import { supabase } from '$core/services/supabase';
-// import { NavItemModel } from '$models/classes/nav-item.model';
+import { environment } from '$environment';
+import { supabase } from '$core/services/supabase';
+import { NavItemModel } from '$models/classes/nav-item.model';
 
 export const get: RequestHandler = async () => {
-	// const res = await supabase.from('navigation').select('*');
-	// const data = res.data ? res.data.map((value) => new NavItemModel().deserialize(value)) : null;
+	if (environment.debug) return { body: JSON.stringify(navigationData) };
+
+	const { data: navigation, error } = await supabase.from('navigation').select('*');
+
+	if (error) {
+		return {
+			status: Number(error.code),
+			body: error.message
+		};
+	}
+
+	const navItems: NavItemModel[] = navigation.map((value) => new NavItemModel().deserialize(value));
 
 	return {
-		body: JSON.stringify(navigationData)
+		body: JSON.stringify(navItems)
 	};
 };
