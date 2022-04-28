e896c14c3d3cf853ab0f31a96976a5cc8e242f6d
vhs <vhsdev@tutanota.com>
2022-04-28 22:45:33 +0800

    refactor: ♻️ harden error page type checking

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,16 +1,14 @@
 <script lang="ts" context="module">
 	import type { Load } from '@sveltejs/kit';
 
-	export const load: Load = ({ error, status }) => {
-		const message = error.message.replace(/:(.*?)$/, '');
-
-		return { props: { status, message } };
-	};
+	export const load: Load = ({ error, status }) => ({ props: { status, error } });
 </script>
 
 <script lang="ts">
-	export let status: string;
-	export let message: string;
+	export let status: number;
+	export let error: Error;
+
+	const summary = error.message.replace(/:(.*?)$/, '');
 </script>
 
 <div class="items-top relative flex min-h-screen justify-center sm:items-center sm:pt-0">
@@ -19,9 +17,8 @@
 			<div class="border-r border-gray-400 px-4 text-lg tracking-wider text-gray-500">
 				{status}
 			</div>
-
 			<div class="ml-4 text-lg uppercase tracking-wider text-gray-500">
-				{message}
+				{summary}
 			</div>
 		</div>
 	</div>
