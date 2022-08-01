ea75abe69017e1d22670ce31f2542c081e9ec453
vhs <vhsdev@tutanota.com>
2022-07-31 19:57:16 -0500

    chore(deps): 🔗 upgrade to tailwind 3.1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const typography = require('@tailwindcss/typography');
 const forms = require('@tailwindcss/forms');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
